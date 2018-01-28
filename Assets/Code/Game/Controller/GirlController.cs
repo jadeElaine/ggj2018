b8c4dfa7f653d74626acc27aab8e9d93db2027ef
jadeElaine <jade52blue@gmail.com>
2018-01-28 00:02:56 -0800

    Frog update & Girl controller first pass

--- Assets/Code/Game/Controller/GirlController.cs
+++ Assets/Code/Game/Controller/GirlController.cs
@@ -14,6 +14,10 @@ public class GirlController {
 	private GirlHub _hub;
 	private float _initialZoom;
 	private float _zoomLevel = 1.0f;
+	private Vector3 _camOffset = new Vector3 ( 0.0f, 2.0f, 1.25f );
+	private Vector3 _camSnapOval = new Vector3 ( 1.0f, 0.0f, 1.4f );
+
+	private FrogHub _heldFrog = null;
 
 	public GirlController( GirlHub hub )
 	{
@@ -23,7 +27,7 @@ public class GirlController {
 		Transform gt = _hub.transform;
 
 		ct.transform.parent.position = _hub.transform.position;
-		ct.transform.position = gt.position + new Vector3 (0, 2.25f, 1);
+		ct.transform.position = gt.position + _camOffset;
 		_initialZoom = (gt.position - ct.transform.position).magnitude;
 		ct.transform.LookAt( gt );
 	}
@@ -34,6 +38,46 @@ public class GirlController {
 		Vector3 mv = new Vector3(Input.GetAxis(m_horizontalWalk), 0, Input.GetAxis(m_verticalWalk)).normalized + Vector3.up*1.0f;
 		_hub.GetComponent<CharacterController> ().Move( -mv * _hub.m_walkSpeed * dt );
 		Vector3 newPosition = _hub.transform.position;
+
+		if ((newPosition - oldPosition).sqrMagnitude > Mathf.Epsilon) {
+			Vector3 facing = (newPosition - oldPosition).normalized;
+			_hub.transform.rotation.SetLookRotation (facing, Vector3.up);
+		}
+
+		if (Input.GetButton (m_interact)) {
+			if (_heldFrog == null) {
+				// find frogs
+				FrogHub[] _allHubs = GameObject.FindObjectsOfType<FrogHub>();
+				FrogHub pickup = null;
+				float pickupDistance = 0.0f;
+
+				for (int i = 0; i < _allHubs.Length; ++i) {
+					float dist = (_allHubs [i].transform.position - _hub.transform.position).magnitude;
+					if (dist < _allHubs [i].m_pickupRange && _allHubs [i].m_canPickUp) {
+						if (pickup == null || dist < pickupDistance) {
+							pickup = _allHubs [i];
+							pickupDistance = dist;
+						}
+					}
+				}
+
+				if (pickup != null) {
+					_heldFrog.transform.SetParent (_hub.m_frogBone);
+					_heldFrog.transform.localPosition = Vector3.zero;
+					_heldFrog.transform.localRotation = Quaternion.identity;
+					_heldFrog.OnPickUp ();
+					_heldFrog = pickup;
+				}
+
+			} else {
+				Transform ground = _hub.transform.parent;
+				_heldFrog.transform.SetParent (ground);
+				_heldFrog.transform.position = _hub.transform.position + _hub.transform.forward * 0.1f;
+				_heldFrog.transform.rotation = _hub.transform.rotation;
+				_heldFrog.OnDrop ();
+				_heldFrog = null;
+			}
+		}
 	}
 	public void UpdateFrameLate( float dt )
 	{
@@ -45,16 +89,16 @@ public class GirlController {
 		at.position += -mv * dt;
 
 		_zoomLevel = Mathf.Clamp (_zoomLevel + Input.GetAxis(m_zoom)*2.5f*dt, 1.0f, 3.0f);
-		ct.position = at.position + new Vector3 (0, 2.25f, 1) * _zoomLevel;
+		ct.position = at.position + _camOffset * _zoomLevel;
 
 		Vector3 spread = (gt.position - at.position);
-		spread.Scale( new Vector3( 1.0f, 0.0f, 1.4f ) );
+		spread.Scale( _camSnapOval );
 		float realZoom = 1.65f * _zoomLevel;
 
 		if (spread.magnitude > realZoom) {
 
 			Vector3 offset = (gt.position - at.position).normalized;
-			offset.Scale (new Vector3 (1.0f, 0.0f, 1.4f));
+			offset.Scale( _camSnapOval );
 			offset *= (spread.magnitude - realZoom);
 			at.position = at.position + offset;
 		}
--- Assets/Code/Game/Support/FrogHub.cs
+++ Assets/Code/Game/Support/FrogHub.cs
@@ -18,4 +18,42 @@ public class FrogHub : MonoBehaviour {
 	public Transform m_upperJawBone;
 	public Transform m_leftBrowBone;
 	public Transform m_rightBrowBone;
+
+	public AudioClip[] m_audioScreams;
+	public AudioClip[] m_audioPickups;
+	public AudioClip[] m_audioDrops;
+
+	private AudioSource _source;
+
+	void Start()
+	{
+		_source = GetComponent<AudioSource> ();
+	}
+
+	public void OnScream()
+	{
+		if( m_audioScreams.Length == 0 ) return;
+		int index = UnityEngine.Random.Range( 0, m_audioScreams.Length );
+		if (_source.isPlaying) { _source.Stop (); }
+		_source.clip = m_audioScreams [index];
+		_source.Play ();
+	}
+
+	public void OnPickUp()
+	{
+		if( m_audioPickups.Length == 0 ) return;
+		int index = UnityEngine.Random.Range( 0, m_audioPickups.Length );
+		if (_source.isPlaying) { _source.Stop (); }
+		_source.clip = m_audioPickups [index];
+		_source.Play ();
+	}
+
+	public void OnDrop()
+	{
+		if( m_audioDrops.Length == 0 ) return;
+		int index = UnityEngine.Random.Range( 0, m_audioDrops.Length );
+		if (_source.isPlaying) { _source.Stop (); }
+		_source.clip = m_audioDrops [index];
+		_source.Play ();
+	}
 }
